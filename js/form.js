912b53f0316345b6b6ee3fd995d76fcae34018a8
Zion Gonet <ziongonet@gmail.com>
2021-08-12 23:21:13 +0100

    Initial Commit of Simple Space Exploration Website

---
+++ js/form.js
@@ -0,0 +1,13 @@
+const submit = document.querySelector('button');
+const form = document.querySelector('form');
+
+
+submit.addEventListener('click', ()=> {
+    alert('Are you sure you want to submit?');
+    alert('Form Sucessfully submitted!');
+
+    form.inputMode.reset = true;
+
+
+})
+
